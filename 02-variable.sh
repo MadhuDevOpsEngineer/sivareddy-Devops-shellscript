0fc9d741827e01675b88e3049ccb37ead391c32c
Madhu <madhudevops2025@gmail.com>
2025-09-05 20:41:36 +0530

    02 file added

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-echo "Ramesh: Hello suresh, how are you?"
-echo "Suresh: I am good, how are you?"
-echo "Ramesh: I too good"
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1: Hello $PERSON2, how are you?"
+echo "$PERSON2: I am good, how are you?"
+echo "$PERSON1: I too good"
