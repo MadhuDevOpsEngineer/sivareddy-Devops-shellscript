0ab7416460303181efee76bba8de5de34dc98711
Madhu <madhudevops2025@gmail.com>
2025-09-06 08:34:47 +0530

    02 file added

---
+++ 10-functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+ID=$(id -u)
+VALIDATE() {
+if [ $? is -ne 0]
+then 
+    echo "ERROR:: Installation of is failed"
+    exit 1
+else
+    echo "Installation of is success"
+fi
+
+}
+if [ $ID -ne 0]
+then 
+  echo "ERROR:: this is not the root user. Please run with root access"
+  exit 1
+else
+   echo "you are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
+
+yum install git -y
+
+VALIDATE
