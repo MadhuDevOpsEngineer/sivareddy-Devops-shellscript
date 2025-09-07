71c7edf591f05cd25981a8829f4056176850a819
Madhu <madhudevops2025@gmail.com>
2025-09-07 21:00:51 +0530

    install package added

---
+++ roboshop-shell/delete_old_logfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SOURCE_DIR=/tmp/shellscript-logs
+if [ ! -d $SOURCE_DIR ]
+then
+  echo -e "Source directory $SOURCE_DIR does not exist"
+fi
+
+FILES_TO_DELETE=$(find . -type f -mtime +14 -name "*.log")
+
+
+while IFS= read -r line
+do 
+  echo "deleting file $line
+done <<< $FILES_TO_DELETE
